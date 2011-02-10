5acc231fc1bbbff8df85365d0c7b6de066a903f0
Sergey Solovyev <serso1988@gmail.com>
2011-02-10 17:51:50 +0300

    generic configuration

---
+++ src/main/java/org/solovyev/common/db/XBasicDataSource.java
@@ -0,0 +1,19 @@
+package org.solovyev.common.db;
+
+import org.apache.commons.dbcp.BasicDataSource;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * User: serso
+ * Date: 2/10/11
+ * Time: 5:02 PM
+ */
+public class XBasicDataSource extends BasicDataSource {
+    @Override
+    public synchronized void close() throws SQLException {
+        DriverManager.deregisterDriver(DriverManager.getDriver(url));
+        super.close();
+    }
+}
