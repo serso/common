993d5e3afa26485de176f3c6611cac8d24d18b5e
Sergey Solovyev <serso1988@gmail.com>
2011-09-19 16:24:37 +0400

    valuew added

---
+++ src/main/java/org/solovyev/common/utils/NumberValuer.java
@@ -0,0 +1,17 @@
+package org.solovyev.common.utils;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * User: serso
+ * Date: 9/19/11
+ * Time: 4:23 PM
+ */
+public class NumberValuer<T extends Number> implements Valuer<T> {
+
+	@Override
+	@NotNull
+	public Double getValue(@NotNull T item) {
+		return item.doubleValue();
+	}
+}
---
+++ src/main/java/org/solovyev/common/utils/Valuer.java
@@ -0,0 +1,19 @@
+package org.solovyev.common.utils;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * User: serso
+ * Date: 9/19/11
+ * Time: 4:23 PM
+ */
+public interface Valuer<T> {
+
+	/**
+	 * @param item object for which value is counted
+	 * @return value of the object
+	 */
+	@NotNull
+	public Double getValue (@NotNull T item);
+
+}
