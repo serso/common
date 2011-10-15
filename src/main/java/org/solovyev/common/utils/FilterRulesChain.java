f5551aba3a4bdbd0b4e50d4d61f992e5ea52bc7e
serso <serso@serso-laptop.(none)>
2011-10-15 14:05:42 +0400

    history changes

---
+++ src/main/java/org/solovyev/common/utils/FilterRulesChain.java
@@ -0,0 +1,38 @@
+package org.solovyev.common.utils;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User: serso
+ * Date: 10/15/11
+ * Time: 2:03 PM
+ */
+public class FilterRulesChain<T> implements FilterRule<T> {
+
+	@NotNull
+	private final List<FilterRule<T>> filters = new ArrayList<FilterRule<T>>();
+
+	public FilterRulesChain() {
+	}
+
+	public void addFilterRule(@NotNull FilterRule<T> filterRule) {
+		filters.add(filterRule);
+	}
+
+	@Override
+	public boolean isFiltered(T object) {
+		boolean result = false;
+
+		for (FilterRule<T> filter : filters) {
+			if (filter.isFiltered(object)) {
+				result = true;
+				break;
+			}
+		}
+
+		return result;
+	}
+}
