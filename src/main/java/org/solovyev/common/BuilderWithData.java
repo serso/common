3a7acda26e91eaa61417b8c5d148db624ae90d73
Sergey Solovyev <se.solovyev@gmail.com>
2012-04-28 01:46:32 +0400

    Another builder added

--- src/main/java/org/solovyev/common/BuilderWithData.java
+++ src/main/java/org/solovyev/common/BuilderWithData.java
@@ -5,10 +5,10 @@ import org.jetbrains.annotations.NotNull;
 /**
  * User: serso
  * Date: 4/28/12
- * Time: 1:46 AM
+ * Time: 1:42 AM
  */
-public interface Builder<T> {
+public interface BuilderWithData<T, D> {
 
     @NotNull
-    T build();
+    T build(D data);
 }
