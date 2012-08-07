add49b21bf87ef5f4f181fcd106dd7056c9552a6
Sergey Solovyev <se.solovyev@gmail.com>
2012-08-08 01:37:51 +0400

    Comments

--- core/src/main/java/org/solovyev/common/utils/CloneUtils.java
+++ core/src/main/java/org/solovyev/common/utils/CloneUtils.java
@@ -1,5 +1,6 @@
 package org.solovyev.common.utils;
 
+import org.jetbrains.annotations.NotNull;
 import org.solovyev.common.JCloneable;
 
 import java.util.List;
@@ -10,21 +11,37 @@ import java.util.ArrayList;
  * Date: 13.04.2009
  * Time: 11:26:54
  */
-public class CloneUtils {
-    public static <T extends JCloneable<T>> List<T> deepListCloning( List<T> source ) {
-        List<T> result = new ArrayList<T>();
+
+/**
+ * Class contains utility methods for cloning
+ */
+public final class CloneUtils {
+
+    private CloneUtils() {
+        throw new AssertionError("Not intended for instantiation!");
+    }
+
+    @NotNull
+    public static <T extends JCloneable<T>> List<T> deepListCloning( @NotNull List<T> source ) {
+        final List<T> result = new ArrayList<T>(source.size());
+
+        // clone all elements in the list
         for ( T element: source ) {
             result.add( element.clone() );
         }
+
         return result;
     }
 
-    @SuppressWarnings("unchecked")
-    public static <T extends JCloneable<T>> T[] deepArrayCloning( T[] array ) {
-        Object[] result = new Object[array.length];
+    @NotNull
+    public static <T extends JCloneable<T>> T[] deepArrayCloning( @NotNull T[] array ) {
+        final T[] result = (T[]) new Object[array.length];
+
+        // clone all elements of array
         for ( int i = 0; i < array.length; i++ ) {
             result[i] = array[i].clone();
         }
-        return (T[])result;
+
+        return result;
     }
 }
