15a0afd5721fee18d2c0a962848ae4514649b6ea
Sergey Solovyev <se.solovyev@gmail.com>
2012-08-12 13:19:57 +0400

    Version entity

---
+++ core/src/main/java/org/solovyev/common/VersionedEntity.java
@@ -0,0 +1,25 @@
+package org.solovyev.common;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.Serializable;
+
+/**
+ * User: serso
+ * Date: 4/29/12
+ * Time: 9:45 PM
+ */
+public interface VersionedEntity<I> extends Serializable, JCloneable<VersionedEntity<I>> {
+
+    @NotNull
+    I getId();
+
+    @NotNull
+    Integer getVersion();
+
+    // check if two entities are the same == this.id equals to that.id
+    boolean equals(Object that);
+
+    // check if two entities are the same version == this.id equals to that.id && this.version equals to that.version
+    boolean equalsVersion(Object that);
+}
