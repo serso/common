256f27b60dc979fc45aea5c8ff3e4bd7744f3660
Sergey Solovyev <se.solovyev@gmail.com>
2012-08-21 14:47:20 +0400

    Map clonning added

--- core/src/main/java/org/solovyev/common/clone/CloneUtils.java
+++ core/src/main/java/org/solovyev/common/clone/CloneUtils.java
@@ -3,8 +3,10 @@ package org.solovyev.common.clone;
 import org.jetbrains.annotations.NotNull;
 import org.solovyev.common.JCloneable;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * User: serso
@@ -22,23 +24,74 @@ public final class CloneUtils {
     }
 
     @NotNull
-    public static <T extends JCloneable<T>> List<T> deepListCloning( @NotNull List<T> source ) {
-        final List<T> result = new ArrayList<T>(source.size());
+    public static <T extends JCloneable<T>> List<T> cloneList(@NotNull List<T> source) {
+        return deepListCloning(source, new ArrayList<T>(source.size()));
+    }
 
+    @NotNull
+    public static <T extends JCloneable<T>> List<T> cloneList(@NotNull List<T> source,
+                                                              @NotNull List<T> destination) {
+        return deepListCloning(source, destination);
+    }
+
+    @NotNull
+    public static <T extends JCloneable<T>> List<T> deepListCloning(@NotNull List<T> source) {
+        return deepListCloning(source, new ArrayList<T>(source.size()));
+    }
+
+    @NotNull
+    public static <T extends JCloneable<T>> List<T> deepListCloning(@NotNull List<T> source,
+                                                                    @NotNull List<T> destination) {
         // clone all elements in the list
-        for ( T element: source ) {
-            result.add( element.clone() );
+        for (T element : source) {
+            destination.add(element.clone());
+        }
+
+        return destination;
+    }
+
+    @NotNull
+    public static <K, V extends JCloneable<V>> Map<K, V> cloneMap(@NotNull Map<K, V> source) {
+        return cloneMap(source, new HashMap<K, V>(source.size()));
+    }
+
+    @NotNull
+    public static <K, V extends JCloneable<V>> Map<K, V> cloneMap(@NotNull Map<K, V> source, @NotNull Map<K, V> destination) {
+        for (Map.Entry<K, V> entry : source.entrySet()) {
+            final V value = entry.getValue();
+            if (value != null) {
+                destination.put(entry.getKey(), value.clone());
+            } else {
+                destination.put(entry.getKey(), null);
+            }
+        }
+
+        return destination;
+    }
+
+    @NotNull
+    public static <K extends JCloneable<K>, V extends JCloneable<V>> Map<K, V> cloneMapWithKeys(@NotNull Map<K, V> source) {
+        final Map<K, V> result = new HashMap<K, V>(source.size());
+
+        for (Map.Entry<K, V> entry : source.entrySet()) {
+            final V value = entry.getValue();
+            final K key = entry.getKey();
+            if (value != null) {
+                result.put(key.clone(), value.clone());
+            } else {
+                result.put(key.clone(), null);
+            }
         }
 
         return result;
     }
 
     @NotNull
-    public static <T extends JCloneable<T>> T[] deepArrayCloning( @NotNull T[] array ) {
+    public static <T extends JCloneable<T>> T[] deepArrayCloning(@NotNull T[] array) {
         final T[] result = (T[]) new Object[array.length];
 
         // clone all elements of array
-        for ( int i = 0; i < array.length; i++ ) {
+        for (int i = 0; i < array.length; i++) {
             result[i] = array[i].clone();
         }
 
